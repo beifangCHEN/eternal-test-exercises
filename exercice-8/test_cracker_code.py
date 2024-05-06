dba42524aceeef754b54595c381dba052e49e79e
CHEN Beifang <11925670@o31018.iutv.univ-paris13.fr>
2024-05-06 14:13:14 +0200

    exo8_corr

--- exercice-8/test_cracker_code.py
+++ exercice-8/test_cracker_code.py
@@ -21,13 +21,76 @@ message_a_cracker = "ZX SXCYXE MJDY CYKCIXY DW CJVX CK YXSEX VX CJWELWDXY K XSSK
 
 hash_du_message_dechiffre = 'b9aff3021b039a5923d11cb941d314c5ca0acd02'
 
+from pprint import pprint
 import hashlib
+super_map_of_characters = {' ': ' ',
+ 'A': 'K',
+ 'B': 'F',
+ 'C': 'C',
+ 'D': 'V',
+ 'E': 'X',
+ 'F': 'H',
+ 'G': 'B',
+ 'H': 'A',
+ 'I': 'L',
+ 'J': 'N',
+ 'K': 'I',
+ 'L': 'Z',
+ 'M': 'U',
+ 'N': 'W',
+ 'O': 'J',
+ 'P': 'M',
+ 'Q': 'O',
+ 'R': 'Y',
+ 'S': 'S',
+ 'T': 'E',
+ 'U': 'D',
+ 'V': 'P',
+ 'W': 'Q',
+ 'X': 'R',
+ 'Y': 'G',
+ 'Z': 'T'}
 
 def encrypt_text(cleartext: str) -> str:
-    raise NotImplementedError()
+    
+    #map_of_characters = dict(zip(message_en_clair, message_chiffre))
+    #print(map_of_characters)
+    #alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+    #map_of_characters['L'] = 'Z'
+    #map_of_characters['K'] = 'I'
+    #map_of_characters['Y'] = 'G'
+#
+    #missing_source = [a for a in alphabet if a not in map_of_characters]
+    #missing_dest = [a for a in alphabet if a not in map_of_characters.values()]
+    #map_of_characters.update(dict(zip(missing_source, missing_dest)))
+    #pprint(map_of_characters)
+    #print("missing source", missing_source)
+    #print("missing dest", missing_dest)
+    map_of_characters = super_map_of_characters
+    s = ""
+    for c in cleartext:
+        if c not in map_of_characters:
+            raise ValueError(f"cannot encrypt because bad character [{c}]")
+        s += map_of_characters[c]
+    return s
+
+print(encrypt_text('BONJOUR LES AMIS'))
 def decrypt_text(encrypted: str) -> str:
-    raise NotImplementedError()
+    #map_of_characters = dict(zip(message_chiffre, message_en_clair))
+    #map_of_characters['Z'] = 'L'
+    #map_of_characters['I'] = 'K'
+    #map_of_characters['G'] = 'Y'
+    map_of_characters = {v: k for k, v in super_map_of_characters.items()}
+    s = ""
+    
+    for c in encrypted:
+        if c in map_of_characters:
+            s += map_of_characters[c]
+        else:
+            s += f'[{c}]'
+    print(s)
+    return s
 
 def test_crackage():
     assert decrypt_text(message_chiffre) == message_en_clair
