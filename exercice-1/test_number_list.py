3ea6f691fb1905beca77e5f0183afc6da7a5a26a
CHEN Beifang <11925670@o31018.iutv.univ-paris13.fr>
2024-05-06 13:58:00 +0200

    exo1_corr

--- exercice-1/test_number_list.py
+++ exercice-1/test_number_list.py
@@ -1,3 +1,5 @@
+import pytest
+
 def convert_to_integer(input_list: list) -> list:
   """Cette fonction parcourt la liste donnée en entrée et
   convertit tous les éléments qui peuvent l'être en entiers.
@@ -15,6 +17,6 @@ def convert_to_integer(input_list: list) -> list:
   return output_list
 
 def test_convert_to_integer():
-    assert convert_to_integer([1, 2, 3, 4]) == [1, 2, 3, 4]
-
-test_convert_to_integer()
+  assert convert_to_integer([0, 1]) == [0, 1]
+  assert convert_to_integer('0123') == [0, 1, 2, 3]
+  assert convert_to_integer(['a']) == []
